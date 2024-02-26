640db99041bd7c6591210237bfc3178db0d2c6f7
sosyjin <sosyjin@gmail.com>
2024-02-26 15:00:26 +0900

    linux with VSC practice

---
+++ c/add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1 = 0, num2 = 0;
+	printf("Input Two Number: ");
+  scanf("%d %d", &num1, &num2);
+  print("%d + %d = %d\n", num1, num2, num1+num2);
+
+	return 0;
+}
